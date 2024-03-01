0fecf07c3fa4571f3de22c78ccc276b502cdb7c6
parsamre <parsa.mre@gmail.com>
2024-03-01 01:30:03 -0500

    add config

--- config.py
+++ config.py
@@ -4,6 +4,15 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
+    PROPAGATE_EXCEPTIONS = True
+    API_TITLE = "Stores REST API"
+    API_VERSION = "v1"
+    OPENAPI_VERSION = "3.0.3"
+    OPENAPI_URL_PREFIX = "/"
+    OPENAPI_SWAGGER_UI_PATH = "/swagger-ui"
+    OPENAPI_SWAGGER_UI_URL = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+
+
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
