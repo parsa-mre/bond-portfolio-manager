83e8f22509fa4ca8f679f7c4cf6c7007a51eb7e6
parsamre <parsa.mre@gmail.com>
2024-03-01 01:30:47 -0500

    fix app __init__

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from config import Config
 from app.extensions import db
+from flask_smorest import Api
 
 
 def create_app(config_class=Config):
@@ -8,14 +9,20 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
 
     # Initialize Flask extensions here
+    from app.models import Bond, Issuer
+
     db.init_app(app)
+    with app.app_context():
+        db.create_all()
 
     # Register blueprints here
+    api = Api(app)
+
     from app.bonds import blp as bonds_blp
-    app.register_blueprint(bonds_blp)
+    api.register_blueprint(bonds_blp)
 
     from app.issuers import blp as issuers_blp
-    app.register_blueprint(issuers_blp)
+    api.register_blueprint(issuers_blp)
 
     @app.route('/test/')
     def test_page():
